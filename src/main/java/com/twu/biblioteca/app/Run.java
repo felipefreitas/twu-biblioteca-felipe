68acc2ca55819690287a1fa67ec8270b9959bb98
Felipe Freitas <felipesafreitas@gmail.com>
2020-02-04 18:18:02 -0300

    add gradle task to run and
    - call welcome message from console app.
    - remove old biblioteca app assignment.

--- src/main/java/com/twu/biblioteca/app/Run.java
+++ src/main/java/com/twu/biblioteca/app/Run.java
@@ -1,4 +1,11 @@
 package com.twu.biblioteca.app;
 
+import com.twu.biblioteca.view.ConsoleApplication;
+
 public class Run {
+    public static void main(String[] args) {
+        ConsoleApplication consoleApplication = new ConsoleApplication(System.out);
+
+        consoleApplication.showWelcomeMessage();
+    }
 }
