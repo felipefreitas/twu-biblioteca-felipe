70b1c4066fd8ae5e6b64d1f755d57294acb4653b
Felipe Freitas <felipesafreitas@gmail.com>
2020-02-04 17:29:38 -0300

    make test should welcome message pass

--- src/main/java/com/twu/biblioteca/view/ConsoleApplication.java
+++ src/main/java/com/twu/biblioteca/view/ConsoleApplication.java
@@ -4,9 +4,13 @@ import java.io.PrintStream;
 
 public class ConsoleApplication {
 
-    public ConsoleApplication(PrintStream printStreamMock) {}
+    private PrintStream _printStream;
+
+    public ConsoleApplication(PrintStream printStream) {
+        this._printStream = printStream;
+    }
 
     public void showWelcomeMessage() {
-
+        this._printStream.println("Welcome to Biblioteca. Your one-stop-shop for great books titles in Bangalore!");
     }
 }
