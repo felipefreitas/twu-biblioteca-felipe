da11a43e25e537cb4b651b7a96ec59f7a771c924
Felipe Freitas <felipesafreitas@gmail.com>
2020-02-06 01:48:36 -0300

    add to string method to book

--- src/main/java/com/twu/biblioteca/Book.java
+++ src/main/java/com/twu/biblioteca/Book.java
@@ -1,5 +1,14 @@
 package com.twu.biblioteca;
 
 public class Book {
-    
+    private String name;
+    private String author;
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + this.name + '\'' +
+                ", author='" + this.author + '\'' +
+                '}';
+    }
 }
